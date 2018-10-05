b8aaaee545f163a2751b0dc9fc25d12e26137de3
李艳其 <1584329729@qq.com>
2018-10-05 09:23:56 +0800

    添加项目文件。

---
+++ lyq.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace lyq.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ lyq.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lyq.Web.WebApiApplication" Language="C#" %>
