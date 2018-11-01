77116dfbeb701d188e15f2d21c59b8e128657afc
李艳其 <1584329729@qq.com>
2018-11-02 07:38:06 +0800

    201811.2

---
+++ lyq.Infrastructure/Log/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lyq.Infrastructure.Log
+{
+    public interface ILogger
+    {
+        void CreateLogger(Type type);
+        void Debug(object message);
+        void Error(object message);
+        void Info(object message);
+        void Warn(object message);
+    }
+}
---
+++ lyq.Infrastructure/Log/Logger.cs
@@ -0,0 +1,41 @@
+﻿using log4net;
+using System;
+using System.IO;
+using System.Web;
+
+namespace lyq.Infrastructure.Log
+{
+    public class Logger : ILogger
+    {
+        private ILog logger;
+        public Logger()
+        {
+            logger = LogManager.GetLogger(GetType());
+        }
+        static Logger()
+        {
+            FileInfo configFile = new FileInfo(HttpContext.Current.Server.MapPath("/bin/Log/log4net.config"));
+            log4net.Config.XmlConfigurator.Configure(configFile);
+        }
+        public void CreateLogger(Type type)
+        {
+            logger = LogManager.GetLogger(type);
+        }
+        public void Debug(object message)
+        {
+            logger.Debug(message);
+        }
+        public void Error(object message)
+        {
+            logger.Error(message);
+        }
+        public void Info(object message)
+        {
+            logger.Info(message);
+        }
+        public void Warn(object message)
+        {
+            logger.Warn(message);
+        }
+    }
+}
