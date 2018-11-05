43663fe7a07a6173446ebd24d9e671430b8d2bb9
李艳其 <1584329729@qq.com>
2018-11-05 21:45:25 +0800

    20181105

---
+++ lyq.Infrastructure/Log/LoggerConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Web;
+
+namespace lyq.Infrastructure.Log
+{
+    public class LoggerConfig
+    {
+        /// <summary>
+        /// 注册Log4net
+        /// </summary>
+        /// <param name="configPath">配置文件路径</param>
+        public static void RegisterLog4net(string configPath)
+        {
+            FileInfo configFile = new FileInfo(HttpContext.Current.Server.MapPath(configPath));
+            log4net.Config.XmlConfigurator.Configure(configFile);
+        }
+    }
+}
