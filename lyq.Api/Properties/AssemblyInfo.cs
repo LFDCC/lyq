fe9e1b26ef1732ac97e2b722fba3449bcbfd8a26
李艳其 <1584329729@qq.com>
2018-11-22 08:49:51 +0800

    test

--- lyq.Api/Properties/AssemblyInfo.cs
+++ lyq.Api/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
-[assembly: Guid("1f831a3e-ca41-46e5-8bf7-bae15b9c697a")]
+[assembly: Guid("8175f73c-eb53-4fa1-bc38-7498c22941db")]
 
 // 程序集的版本信息由下列四个值组成:
 //
